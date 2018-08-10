8a4bfead4336c48d5dc403f9f4635e06be8fe707
chroda <chroda@chroda.com.br>
2018-08-10 14:46:07 -0300

    ex2 saved

---
+++ udemy-course/webpack-exercises/ex.2/index.js
@@ -0,0 +1,4 @@
+import People from './people';
+
+const people = new People('chroda');
+console.log(people.toString());
---
+++ udemy-course/webpack-exercises/ex.2/people.js
@@ -0,0 +1,9 @@
+export default class People {
+  constructor(name) {
+    this.name = name
+  }
+
+  toString() {
+    return `People: ${this.name}`
+  }
+}
