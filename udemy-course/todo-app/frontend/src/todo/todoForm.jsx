e8db72445e52f56113e5d03e5668753bc0eaaede
chroda <chroda@chroda.com.br>
2018-08-11 20:38:05 -0300

    shortKeys

--- udemy-course/todo-app/frontend/src/todo/todoForm.jsx
+++ udemy-course/todo-app/frontend/src/todo/todoForm.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-export default props => (
-  <div role='form' className='todoForm'>
-    <Grid cols='12 9 10'>
-      <input id='description'
-        className='form-control'
-        placeholder='add some task'
-        value={props.description}
-        onChange={props.handleChange}
-        />
-    </Grid>
-    <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
-      <IconButton style='info' icon='search' onClick={props.handleSearch} />
-      <IconButton style='default' icon='close' onClick={props.handleClear} />
-    </Grid>
-  </div>
-)
+export default props => {
+  const keyHander = (e) => {
+    if (e.key === 'Enter' ) {
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    } else if (e.key === 'Escape') {
+      props.handleClear()
+    }
+  }
+
+
+  return (
+    <div role='form' className='todoForm'>
+      <Grid cols='12 9 10'>
+        <input id='description'
+          className='form-control'
+          placeholder='add some task'
+          value={props.description}
+          onChange={props.handleChange}
+          onKeyUp={keyHander}
+          />
+      </Grid>
+      <Grid cols='12 3 2'>
+        <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
+        <IconButton style='info' icon='search' onClick={props.handleSearch} />
+        <IconButton style='default' icon='close' onClick={props.handleClear} />
+      </Grid>
+    </div>
+  )
+}
