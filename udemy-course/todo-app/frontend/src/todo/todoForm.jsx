f681dfa878549005b59a10337228b376208facab
chroda <chroda@chroda.com.br>
2018-08-11 17:32:51 -0300

    iconButton componet + fix grid component

--- udemy-course/todo-app/frontend/src/todo/todoForm.jsx
+++ udemy-course/todo-app/frontend/src/todo/todoForm.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
 export default props => (
   <div role='form' className='todoForm'>
-    <div className='col-xs-12 col-sm-9 col-md-10'>
-      <input id='descr' className='form-control' placeholder='add some task'/>
-    </div>
-    <div className='col-xs-12 col-sm-3 col-md-2'>
-      <button className='btn btn-primary'><i className='fa fa-plus'></i></button>
-    </div>
+    <Grid cols='12 9 10'>
+      <input id='description' className='form-control' placeholder='add some task'/>
+    </Grid>
+    <Grid cols='12 3 2'>
+      <IconButton style='primary' icon='plus' />
+    </Grid>
   </div>
 )
