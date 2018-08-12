98d7adc130dbcb3f57c2428de0149c73c4d4ea3b
chroda <chroda@chroda.com.br>
2018-08-11 23:49:23 -0300

    removed trash

--- udemy-course/exercises-react/ex/field.jsx
+++ udemy-course/exercises-react/ex/field.jsx
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Field extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: props.initialValue };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
   render() {
     return (
       <div>
