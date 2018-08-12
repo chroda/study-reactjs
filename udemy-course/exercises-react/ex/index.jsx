691de5ad37a47ba5e97d2676944aceb646dec0f6
chroda <chroda@chroda.com.br>
2018-08-11 23:40:33 -0300

    reducers

--- udemy-course/exercises-react/ex/index.jsx
+++ udemy-course/exercises-react/ex/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-
 import Field from './field';
+
+const reducers = combineReducers({
+  field: () => ({ value: 'Hello' })
+});
 
 ReactDOM.render(
   <Field initialValue='testing' />
