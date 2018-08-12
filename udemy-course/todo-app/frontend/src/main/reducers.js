c274d94c6ea1557b3d2cd5c0fb0047365ae16536
chroda <chroda@chroda.com.br>
2018-08-12 19:22:40 -0300

    update static reducer

--- udemy-course/todo-app/frontend/src/main/reducers.js
+++ udemy-course/todo-app/frontend/src/main/reducers.js
@@ -1,1 +1,26 @@
 import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  todo: () => ({
+    description: 'read a book',
+    list: [
+      {
+        _id: 1,
+        description: 'pay card bill',
+        done: true
+      },
+      {
+        _id: 2,
+        description: 'pay card bill',
+        done: false
+      },
+      {
+        _id: 3,
+        description: 'pay card bill',
+        done: false
+      },
+    ]
+  })
+});
+
+export default rootReducer;
