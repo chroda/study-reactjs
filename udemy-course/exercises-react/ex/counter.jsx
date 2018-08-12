6a78400e82ea4c9262266e86b85efaa486dfd148
chroda <chroda@chroda.com.br>
2018-08-12 15:09:02 -0300

    counter working with redux

--- udemy-course/exercises-react/ex/counter.jsx
+++ udemy-course/exercises-react/ex/counter.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { inc, dec, stepChanged } from './counterActions';
 
 const Counter = props => (
-  <div>ok</div>
+  <div>
+    <h1>{props.counter.number}</h1>
+    <input onChange={props.stepChanged} value={props.counter.step} type='number'/>
+    <button onClick={props.dec}>Dec</button>
+    <button onClick={props.inc}>Inc</button>
+  </div>
 );
 
-export default Counter
+const mapStateToProps = state => ({ counter: state.counter });
+const mapDispatchToProps = dispatch => bindActionCreators({inc, dec, stepChanged}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- udemy-course/exercises-react/ex/index.jsx
+++ udemy-course/exercises-react/ex/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import Field from './field';
-import fieldReducer from './fieldReducer';
+import counterReducer from './counterReducer';
+import Counter from './counter';
 
-const reducers = combineReducers({ field: fieldReducer });
+const reducers = combineReducers({ counter: counterReducer });
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <Field initialValue='testing' />
+    <Counter />
   </Provider>
   , document.getElementById('app'));
