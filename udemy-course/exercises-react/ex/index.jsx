d1d842b2f06199d599fed524d1cd2cd8210c27a5
chroda <chroda@chroda.com.br>
2018-08-11 23:38:16 -0300

    add imports

--- udemy-course/exercises-react/ex/index.jsx
+++ udemy-course/exercises-react/ex/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import Field from './field';
 
 ReactDOM.render(
