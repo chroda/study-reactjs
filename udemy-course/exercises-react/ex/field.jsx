deb16837dec8ebc494e78de664dbdcfb8d350ec8
chroda <chroda@chroda.com.br>
2018-08-11 23:55:53 -0300

    more imports

--- udemy-course/exercises-react/ex/field.jsx
+++ udemy-course/exercises-react/ex/field.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { changeValue } from './fieldActions';
 
 class Field extends Component {
   render() {
     return (
       <div>
         <label>{this.props.value}</label><br />
-        <input onChange={this.handleChange} value={this.props.value} />
+        <input onChange={this.props.changeValue} value={this.props.value} />
       </div>
     )
   }
