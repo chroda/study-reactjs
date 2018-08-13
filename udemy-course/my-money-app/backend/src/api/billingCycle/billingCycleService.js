47eb501f7f7a5761e6093357c03a31bd2a38cdc3
chroda <chroda@chroda.com.br>
2018-08-13 14:40:02 -0300

    create count route

--- udemy-course/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ udemy-course/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,15 @@
 const BillingCycle = require('./billingCycle');
 
 BillingCycle.methods(['get','post','put','delete']);
-
 BillingCycle.updateOptions({new: true, runValidators: true});
+BillingCycle.route('count', (requestAnimationFrame, res, next) => {
+  BillingCycle.count((error, value) => {
+    if(error){
+      res.status(500).json({errors:[error]});
+    } else {
+      res.json({value});
+    }
+  });
+});
 
 module.exports = BillingCycle;
