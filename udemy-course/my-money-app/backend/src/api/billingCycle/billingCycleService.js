8ae3d707dd0a6c8e056dee4f83ca3b7eabe4921a
chroda <chroda@chroda.com.br>
2018-08-13 08:29:17 -0300

    service created

---
+++ udemy-course/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,7 @@
+const BillingCycle = require('./billingCycle');
+
+BillingCycle.methods(['get','post','put','delete']);
+
+BillingCycle.updateOptions({new: true, runValidators: true});
+
+module.exports = BillingCycle;
