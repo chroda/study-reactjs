d00573b52ab87d7802bb27a6691d6e19a82e5357
chroda <chroda@chroda.com.br>
2018-08-12 22:24:00 -0300

    methods done/pending okay

--- udemy-course/todo-app/frontend/src/todo/todoActions.js
+++ udemy-course/todo-app/frontend/src/todo/todoActions.js
@@ -26,7 +26,13 @@ export const add = (description) => {
 export const markAsDone = (todo) => {
   return dispatch => {
     axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-    .then(resp => dispatch({ type: 'TODO_MARKED_AS_DONE', payload: resp.data }))
+    .then(() => dispatch(search()));
+  }
+}
+
+export const markAsPending = (todo) => {
+  return dispatch => {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
     .then(() => dispatch(search()));
   }
 }
--- udemy-course/todo-app/frontend/src/todo/todoList.jsx
+++ udemy-course/todo-app/frontend/src/todo/todoList.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import IconButton from '../template/iconButton';
+import { markAsDone, markAsPending } from './todoActions';
+
 
 const TodoList = props => {
   const renderRows = () => {
@@ -9,8 +13,8 @@ const TodoList = props => {
       <tr key={todo._id}>
         <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
         <td>
-          <IconButton style='success' icon='check' onClick={() => props.handleMarkAsDone(todo)} hide={todo.done}/>
-          <IconButton style='warning' icon='undo' onClick={() => props.handleMarkAsPending(todo)} hide={!todo.done}/>
+          <IconButton style='success' icon='check' onClick={() => props.markAsDone(todo)} hide={todo.done}/>
+          <IconButton style='warning' icon='undo' onClick={() => props.markAsPending(todo)} hide={!todo.done}/>
           <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} hide={!todo.done} />
         </td>
       </tr>
@@ -33,5 +37,6 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({list: state.todo.list});
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending }, dispatch)
 
-export default connect(mapStateToProps)(TodoList);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
