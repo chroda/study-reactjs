03e87188177b07f55a47c99246d8bf091141f476
chroda <chroda@chroda.com.br>
2018-08-12 21:25:32 -0300

    apply Middleware

--- udemy-course/todo-app/frontend/src/index.jsx
+++ udemy-course/todo-app/frontend/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore }from 'redux';
+import { applyMiddleware, createStore }from 'redux';
 import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
 import App from './main/app';
 import reducers from './main/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducers, devTools);
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
