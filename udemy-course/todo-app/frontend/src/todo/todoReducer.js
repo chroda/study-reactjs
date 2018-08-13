1a90eaf2bef35d292faca1a4e8c3437c9c792233
chroda <chroda@chroda.com.br>
2018-08-12 21:26:37 -0300

    removed dumb mocky

--- udemy-course/todo-app/frontend/src/todo/todoReducer.js
+++ udemy-course/todo-app/frontend/src/todo/todoReducer.js
@@ -1,18 +1,4 @@
-const INITIAL_STATE = {
-  description: 'type something',
-  list: [
-    {
-      _id: 1,
-      description: 'an mocked example',
-      done: true
-    },
-    {
-      _id: 2,
-      description: 'another simple mocky',
-      done: false
-    },
-  ]
-};
+const INITIAL_STATE = { description: '', list: [] };
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
