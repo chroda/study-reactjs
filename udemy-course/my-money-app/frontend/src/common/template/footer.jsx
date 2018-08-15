334b2fb7233e852c61b9b0f6ac15f21901a3fb73
chroda <chroda@chroda.com.br>
2018-08-14 23:09:59 -0300

    update footer

--- udemy-course/my-money-app/frontend/src/common/template/footer.jsx
+++ udemy-course/my-money-app/frontend/src/common/template/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default props => (
   <footer className='main-footer'>
     <strong>
-      Copyright &copy; 2017
-      <a href='http://cod3r.com.br' target='_blank'> Cod3r</a>.
+      Copyright &copy; 2018
+      <a href='http://chroda.com.br' target='_blank'> chroda</a>.
     </strong>
   </footer>
 );
