3899fc992336425a666bdfc755f570d45cd93c25
chroda <chroda@chroda.com.br>
2021-11-18 21:40:51 +0000

    todo reducer

--- react-ecosystems/src/store.js
+++ react-ecosystems/src/store.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers } from "redux";
+import todos from "./todos/reducers";
 
-const reducers = {};
+const reducers = {
+  todos,
+};
 
 const rootReducer = combineReducers(reducers);
 
