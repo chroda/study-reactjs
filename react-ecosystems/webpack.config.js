9b991715e5063f3ca559472ef114524555f3118b
chroda <chroda@chroda.com.br>
2021-11-18 17:30:15 +0000

    renamed src folder

--- react-ecosystems/webpack.config.js
+++ react-ecosystems/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
-        options: { preset: ["@babel/env"] },
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.css$/,
@@ -18,7 +18,7 @@ module.exports = {
       },
     ],
   },
-  resolve: { extensions: ["*", "js", "jsx"] },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
@@ -27,7 +27,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist",
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 };
