0b5aa132231900d99b3ef783b460426dda8a0691
chroda <chroda@chroda.com.br>
2021-11-19 01:03:12 +0000

    persisted store

--- react-ecosystems/src/index.js
+++ react-ecosystems/src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
 import { configureStore } from "./store.js";
 import App from "./App.js";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- react-ecosystems/src/store.js
+++ react-ecosystems/src/store.js
@@ -1,10 +1,24 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 import { todos } from "./todos/reducers.js";
 
 const reducers = {
   todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
