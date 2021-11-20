f3c3612f096cb5b1f65d4c05ea7385ad4efb26c2
chroda <chroda@chroda.com.br>
2021-11-20 12:20:22 +0000

    reselect

--- react-ecosystems/src/todos/selector.js
+++ react-ecosystems/src/todos/selector.js
@@ -1,2 +1,12 @@
+import { createSelector } from "reselect";
+
 export const getTodos = (state) => state.todos.data;
 export const getTodosLoading = (state) => state.todos.isLoading;
+
+export const getIncompleteTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => !todo.isCompleted)
+);
+
+export const getCompleteTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => todo.isCompleted)
+);
